514c15f14984ce183f11fd2ee67a61978df8878b
omjinLTS <omjin7g@gmail.com>
2024-06-11 16:40:14 +0900

    fix: meeting page

--- src/pages/lecture/group/MeetingPage.tsx
+++ src/pages/lecture/group/MeetingPage.tsx
@@ -1,5 +1,11 @@
+import useTeamStore from "@/store/useTeamStore";
+
 export default function MeetingPage() {
-  return (
+  const { teammates } = useTeamStore();
+
+  return teammates.length === 0 ? (
+    <h3 style={{ margin: 0 }}>아직 팀이 없습니다</h3>
+  ) : (
     <img
       src='/zoom.png'
       width={"118%"}
